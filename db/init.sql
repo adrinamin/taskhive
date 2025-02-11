f751adcb8df4add6c06ff542a96030d37253fd6e
Adrin Amin Salehi <adrin.amin@gmail.com>
2025-02-11 06:48:15 +0000

    Enhance database initialization script to check for existing user and database before creation; update Docker Compose entrypoint to keep the container running

--- db/init.sql
+++ db/init.sql
@@ -1,13 +1,28 @@
-CREATE LOGIN dev_user WITH PASSWORD = '$(Password)';
+IF NOT EXISTS (SELECT 1
+FROM sys.server_principals
+WHERE name = 'dev_user')
+BEGIN
+    CREATE LOGIN dev_user WITH PASSWORD = '$(Password)';
+END;
 GO
 
-CREATE DATABASE TaskHive;
+IF NOT EXISTS (SELECT 1
+FROM sys.databases
+WHERE name = 'TaskHive')
+BEGIN
+    CREATE DATABASE TaskHive;
+END;
 GO
 
 USE TaskHive;
 GO
 
-CREATE USER taskhive_dev_user FOR LOGIN dev_user;
+IF NOT EXISTS (SELECT 1
+FROM sys.database_principals
+WHERE name = 'taskhive_dev_user')
+BEGIN
+    CREATE USER taskhive_dev_user FOR LOGIN dev_user;
+END;
 GO
 
 ALTER ROLE db_owner ADD MEMBER taskhive_dev_user;
